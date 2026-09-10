8def8df56eb9c4acbd1c5ff5195d4809b6b8d60d
Nhico <23105039@usc.edu.ph>
2026-09-10 12:22:20 +0800

    Updated ver1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,92 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package Ver1;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hourly Employee Test");
+        System.out.println("Employee Test");
 
-        Employee employee1 = new Employee(101, "Alice Smith");
+        HourlyEmployee hourly1 =
+                new HourlyEmployee(101, "Bayot Dev");
+
+        hourly1.setTotalHoursWorked(45);
+        hourly1.setRatePerHour(200);
+
+        HourlyEmployee hourly2 =
+                new HourlyEmployee(102, "John Doe", 35, 180);
+
+        System.out.println("[displayHourlyEmployee()]");
+
+        hourly1.displayHourlyEmployee();
+        hourly2.displayHourlyEmployee();
+
+        System.out.println("[toString()]");
+
+        System.out.println(hourly1);
+        System.out.println(hourly2);
+
+        System.out.println("\nPiece Worker");
+
+        PieceWorkerEmployee piece1 =
+                new PieceWorkerEmployee(201, "Bob Jones");
+
+        piece1.setTotalPiecesFinished(250);
+        piece1.setRatePerPiece(15);
+
+        PieceWorkerEmployee piece2 =
+                new PieceWorkerEmployee(677, "John Dev", 150, 20);
+
+        System.out.println("[displayPieceWorkerEmployee()]");
+
+        piece1.displayPieceWorkerEmployee();
+        piece2.displayPieceWorkerEmployee();
+
+        System.out.println("[toString()]");
+
+        System.out.println(piece1);
+        System.out.println(piece2);
+
+        System.out.println("\nCommission Employee");
+
+        CommissionEmployee commission1 =
+                new CommissionEmployee(767, "Bayot Dev");
+
+        commission1.setTotalSale(75000);
+
+        CommissionEmployee commission2 =
+                new CommissionEmployee(776, "Pisut Dev", 150000);
+
+        System.out.println("[displayCommissionEmployee()]");
+
+        commission1.displayCommissionEmployee();
+        commission2.displayCommissionEmployee();
+
+        System.out.println("[toString()]");
+
+        System.out.println(commission1);
+        System.out.println(commission2);
+
+        System.out.println("\nBase Plus Commission");
+
+        BasePlusCommissionEmployee base1 =
+                new BasePlusCommissionEmployee (401, "Liam Lawson");
+
+        base1.setTotalSale(120000);
+        base1.setBaseSalary(25000);
+
+        BasePlusCommissionEmployee base2 =
+                new BasePlusCommissionEmployee(402, "Kawaii Dev", 600000, 30000);
+
+        System.out.println("[displayBasePlusCommissionEmployee()]");
+
+        base1.displayBasePlusCommissionEmployee();
+        base2.displayBasePlusCommissionEmployee();
+
+        System.out.println("[toString()]");
+
+        System.out.println(base1);
+        System.out.println(base2);
     }
 }
---
+++ src/Ver1/BasePlusCommissionEmployee.java
@@ -0,0 +1,102 @@
+package Ver1;
+
+class BasePlusCommissionEmployee {
+
+    private int empID;
+    private String empName;
+    private double totalSale;
+    private double baseSalary;
+
+    public BasePlusCommissionEmployee() {
+        empID = 0;
+        empName = "N/A";
+        totalSale = 0;
+        baseSalary = 0;
+    }
+
+    public BasePlusCommissionEmployee(int empID, String empName) {
+        this.empID = empID;
+        this.empName = empName;
+        this.totalSale = 0;
+        this.baseSalary = 0;
+    }
+
+    public BasePlusCommissionEmployee(int empID, String empName, double totalSale, double baseSalary) {
+        this.empID = empID;
+        this.empName = empName;
+        this.totalSale = totalSale;
+        this.baseSalary = baseSalary;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public double getTotalSale() {
+        return totalSale;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public void setTotalSale(double totalSale) {
+        if (totalSale >= 0) {
+            this.totalSale = totalSale;
+        }
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary >= 0) {
+            this.baseSalary = baseSalary;
+        }
+    }
+
+    private double getCommissionRate() {
+        if (totalSale < 50000) {
+
+            return 0.05;
+
+        } else if (totalSale < 100000) {
+
+            return 0.10;
+
+        } else if (totalSale < 500000) {
+
+            return 0.15;
+
+        } else {
+
+            return 0.20;
+        }
+    }
+
+    public double computeSalary() {
+
+        return baseSalary +
+                (totalSale * getCommissionRate());
+    }
+
+    public void displayBasePlusCommissionEmployee() {
+
+        System.out.printf("ID: %d | Name: %s | Total Sale: ₱%.2f | Base Salary: ₱%.2f%n", empID, empName, totalSale, baseSalary);
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("BasePlusCommissionEmployee [ID: %d, Name: %s, Sales: ₱%.2f, Base Salary: ₱%.2f, Commission Rate: %.0f%%, Total Salary: ₱%.2f]", empID, empName, totalSale, baseSalary, getCommissionRate() * 100, computeSalary());
+    }
+}
